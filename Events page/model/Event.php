26177d79466ca541226228087844e6f1e41ee87a
aaronkwk <aaron.koh.2024@computing.smu.edu.sg>
2025-10-28 13:31:47 +0800

    model for sql & new file dir for pics

---
+++ Events page/model/Event.php
@@ -0,0 +1,67 @@
+<?php
+
+class Event {
+    private $title;
+    private $category;
+    private $date;
+    private $start_time;
+    private $end_time;
+    private $location;
+    private $picture;
+    private $startISO;
+    private $endISO;
+
+
+    
+    public function __construct($title, $category, $date, $start_time, $end_time, $location, $picture, $startISO, $endISO){
+        $this->title = $title;
+        $this->category = $category;
+        $this->date = $date;
+        $this->start_time = $start_time;
+        $this->end_time = $end_time;
+        $this->location = $location;
+        $this->picture = $picture;
+        $this->startISO = $startISO;
+        $this->endISO = $endISO;
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+    
+    public function getCategory(){
+        return $this->category;
+    }
+
+    public function getDate(){
+        return $this->date;
+    }
+
+    public function getStartTime(){
+        return $this->start_time;
+    }
+
+    public function getEndTime(){
+        return $this->end_time;
+    }
+
+    public function getLocation(){
+        return $this->location;
+    }
+
+    public function getPicture(){
+        return $this->picture;
+    }
+    
+    public function getStartISO(){
+        return $this->startISO;
+    }
+
+    public function getEndISO(){
+        return $this->endISO;
+    }
+    
+}
+
+?>
+
---
+++ Events page/model/EventCollectionDAO.php
@@ -0,0 +1,68 @@
+<?php
+
+class EventCollectionDAO
+{
+    public function getEvents()
+    {
+        $sql = "select * from events";
+
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        $events = [];
+        while ($row = $stmt->fetch()) {
+            $events[] = new Event(
+                $row["title"],
+                $row["category"],
+                $row["date"],
+                $row["start_time"],
+                $row["end_time"],
+                $row["location"],
+                $row["picture"],
+                $row["startISO"],
+                $row["endISO"]
+            );
+        }
+
+        $stmt = null;
+        $conn = null;
+        return $events;
+    }
+
+    public function getFilteredEvents($filter) {
+        $sql = "select * from events where category = :filter";
+
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':filter', $filter, PDO::PARAM_STR);
+
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        $events = [];
+        while ($row = $stmt->fetch()) {
+            $events[] = new Event(
+                $row["title"],
+                $row["category"],
+                $row["date"],
+                $row["start_time"],
+                $row["end_time"],
+                $row["location"],
+                $row["picture"],
+                $row["startISO"],
+                $row["endISO"]
+            );
+        }
+
+        $stmt = null;
+        $conn = null;
+        return $events;
+
+    }
+}
